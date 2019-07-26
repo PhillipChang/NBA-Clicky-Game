a498a3dd378517304850e51bbec5a672400a4e0a
Phillip Chang <phillip-chang@live.com>
2019-07-25 22:58:23 -0700

    Add title component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import LogoCard from "./components/LogoCard";
 import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import logos from "./logo.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    logos
+  };
+
+  render() {
+    const {image,id,name} = logo;
+    return (
+      <Wrapper>
+      <Title> Clicky Game </Title>
+      {this.state.logos.map(logo => (
+        <LogoCard
+        id={id}
+        key={id}
+        name={name}
+        image={image}
+        />
+      ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
